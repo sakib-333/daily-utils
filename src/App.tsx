f68b73f595cc8560e6db1add110fca2bc832fb45
Sakibur Rahman <sakib.cse.333@gmail.com>
2026-06-02 22:55:33 +0600

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500">Hello World!</h1>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default App
