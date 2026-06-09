6a17f385a46fcbe66fc572ef539dae4263e92ee3
Sakibur Rahman <sakib.cse.333@gmail.com>
2026-06-09 23:02:06 +0600

    Add firestore.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,45 @@
 import Footer from '@/components/footer'
 import Navbar from '@/components/Navbar'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { useEffect } from 'react';
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  setDoc,
+  increment,
+} from "firebase/firestore";
+import { db } from "../../firebase.config"
 
 export const Route = createRootRoute({
     component: RootComponent,
 })
 
 function RootComponent() {
+
+    // ───────────────── Firestore Config ─────────────────
+    useEffect(() => {
+        const updateVisitorCount = async () => {
+            if (localStorage.getItem("visited")) return;
+
+            const ref = doc(db, "stats", "visitors");
+
+            const snapshot = await getDoc(ref);
+
+            if (!snapshot.exists()) {
+                await setDoc(ref, { count: 1 });
+            } else {
+                await updateDoc(ref, {
+                    count: increment(1),
+                });
+            }
+
+            localStorage.setItem("visited", "true");
+        };
+
+        updateVisitorCount();
+    }, []);
+
     return (
         <div className="min-h-screen bg-background pt-16 text-foreground">
             <Navbar />
